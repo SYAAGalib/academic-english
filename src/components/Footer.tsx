9ae2d1f7a741f58949628d0d075e20f640d125dc
FatemaNaznin <fatemanazninpriangca@gmail.com>
2025-06-12 18:12:13 +0600

    footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,7 +40,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400 text-sm">
-            © 2024 AcademicEnglishBD. {isBangla ? 'সমস্ত অধিকার সংরক্ষিত।' : 'All rights reserved.'}
+            © 2025 AcademicEnglishBD. {isBangla ? 'সমস্ত অধিকার সংরক্ষিত।' : 'All rights reserved.'}
           </p>
         </div>
       </div>
